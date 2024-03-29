a13a8112def1b66f16a47adee0070c50f7381c2e
Hamid Heyde <heidarzadeh.hamidreza@gmail.com>
2024-03-28 18:17:26 -0700

    removed todoList mock data

--- src/App.js
+++ src/App.js
@@ -2,33 +2,9 @@ import './app.css'
 import {TodoApp} from './features/todoList';
 import {CustomComponents} from './features/customComponents';
 
-// todoList data
-const todoList = [
-  {
-    id: 1,
-    title: "Learn React",
-    completed: false,
-    pinned: true,
-    favorite: false,
-  },
-  {
-    id: 2,
-    title: "Learn Redux",
-    completed: false,
-    pinned: false,
-    favorite: true,
-  },
-  {
-    id: 3,
-    title: "Learn React Router",
-    completed: true,
-    pinned: false,
-    favorite: false,
-  },
-]
 const App = () => (
   <div className="appWrapper">
-    <TodoApp todoList={todoList}/>
+    <TodoApp/>
     <CustomComponents/>
   </div>
 )
