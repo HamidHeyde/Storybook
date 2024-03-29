328a2cb547ee6cf7ce2065589aa4082d2f2a7152
Hamid Heyde <heidarzadeh.hamidreza@gmail.com>
2024-03-28 18:14:55 -0700

    custom components index

---
+++ src/features/customComponents/index.js
@@ -0,0 +1,9 @@
+import './index.css'
+
+export const CustomComponents = () => {
+  return (
+    <div className="customComponentsAppWrapper">
+      <div className="customComponentsWrapper">custom component</div>
+    </div>
+  )
+}
