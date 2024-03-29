7956875f812a44ca1809ae92f97da525c70d75da
Hamid Heyde <heidarzadeh.hamidreza@gmail.com>
2024-03-28 18:13:10 -0700

    added todo, custom components

--- src/App.js
+++ src/App.js
@@ -1,3 +1,36 @@
-const App = () => (<div>Hi</div>)
+import './app.css'
+import {TodoApp} from './features/todoList';
+import {CustomComponents} from './features/customComponents';
+
+// todoList data
+const todoList = [
+  {
+    id: 1,
+    title: "Learn React",
+    completed: false,
+    pinned: true,
+    favorite: false,
+  },
+  {
+    id: 2,
+    title: "Learn Redux",
+    completed: false,
+    pinned: false,
+    favorite: true,
+  },
+  {
+    id: 3,
+    title: "Learn React Router",
+    completed: true,
+    pinned: false,
+    favorite: false,
+  },
+]
+const App = () => (
+  <div className="appWrapper">
+    <TodoApp todoList={todoList}/>
+    <CustomComponents/>
+  </div>
+)
 
 export default App;
