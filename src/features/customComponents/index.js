12c52825daaa5f23547eccfa9832e6274926ef3b
Hamid Heyde <heidarzadeh.hamidreza@gmail.com>
2024-04-05 10:00:00 +0100

    main panel

--- src/features/customComponents/index.js
+++ src/features/customComponents/index.js
@@ -1,10 +1,19 @@
-import './index.css'
+import "./index.css"
+import { useState } from "react"
+import { MapData } from "./data/MapsData"
+import { PanelHeader } from "./components/mainPanel/PanelHeader"
+import { PanelContent } from "./components/mainPanel/PanelContent"
 
 export const CustomComponents = () => {
+  const [maps, setMaps] = useState(MapData || [])
+
   return (
     <div className="customComponentsAppWrapper">
       <div className="customComponentsWrapper">
-        <div className="panelWrapper"></div>
+        <div className="panelWrapper">
+          <PanelHeader />
+          <PanelContent maps={maps} setMaps={setMaps} />
+        </div>
       </div>
     </div>
   )
