b9e149283ec09ae3f92596e3833f05bc0bb11b7d
Hamid Heyde <heidarzadeh.hamidreza@gmail.com>
2024-04-04 17:30:49 -0700

    moved todoList data to a new file

---
+++ src/features/todoList/data/TodoListData.js
@@ -0,0 +1,23 @@
+export const TodoListData = [
+  {
+    id: 1,
+    title: "Learn React",
+    completed: false,
+    pinned: true,
+    favorite: false,
+  },
+  {
+    id: 2,
+    title: "Learn Redux",
+    completed: false,
+    pinned: false,
+    favorite: true,
+  },
+  {
+    id: 3,
+    title: "Learn React Router",
+    completed: true,
+    pinned: false,
+    favorite: false,
+  },
+]
