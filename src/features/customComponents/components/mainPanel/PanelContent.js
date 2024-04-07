3fcf7f038f8f0195c6c7d51f321d0a44bf9fffd6
Hamid Heyde <heidarzadeh.hamidreza@gmail.com>
2024-04-07 10:00:00 +0100

    main panel content component

---
+++ src/features/customComponents/components/mainPanel/PanelContent.js
@@ -0,0 +1,33 @@
+import "./panelContent.css"
+import { useState } from "react"
+import { InfoPanel } from "../infoPanel"
+import { DataPanel } from "../dataPanel"
+import { NewPanel } from "../newPanel"
+
+const getPanel = (panel) => {
+  const stringToComponent = {
+    InfoPanel: InfoPanel,
+    DataPanel: DataPanel,
+    NewPanel: NewPanel,
+  }
+
+  return stringToComponent[panel] || InfoPanel
+}
+
+export const PanelContent = ({ maps, setMaps }) => {
+  const [panel, setPanel] = useState("InfoPanel")
+
+  const Component = getPanel(panel)
+
+  const panelProperties = {
+    ...{ setPanel },
+    ...(panel === "DataPanel" && { maps, setMaps }),
+    ...(panel === "NewPanel" && { maps, setMaps }),
+  }
+
+  return (
+    <div className="panelContent">
+      <Component {...panelProperties} />
+    </div>
+  )
+}
