d5562897a1f54cae3ae3a38d30292400804de92a
Hamid Heyde <heidarzadeh.hamidreza@gmail.com>
2024-04-08 14:27:27 -0700

    data panel header

---
+++ src/features/customComponents/components/dataPanel/DataPanelHeader.js
@@ -0,0 +1,11 @@
+import './dataPanelHeader.css'
+
+export const DataPanelHeader = () => {
+  return (
+    <div className="dataPanelHeaderWrapper">
+      <div className="headerTop">
+        <div className="headerTab">Active</div>
+      </div>
+    </div>
+  )
+}
