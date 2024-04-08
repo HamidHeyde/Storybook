544f94a59aaea6b4f3f79a8a113cd1b550d502bb
Hamid Heyde <heidarzadeh.hamidreza@gmail.com>
2024-04-08 14:25:09 -0700

    data panel main component

---
+++ src/features/customComponents/components/dataPanel/index.js
@@ -0,0 +1,12 @@
+import "./index.css"
+import { DataPanelHeader } from "./DataPanelHeader"
+import { DataPanelContent } from "./DataPanelContent"
+
+export const DataPanel = ({ setPanel, maps, setMaps }) => {
+  return (
+    <div className="dataPanelWrapper">
+      <DataPanelHeader />
+      <DataPanelContent setPanel={setPanel} maps={maps} setMaps={setMaps} />
+    </div>
+  )
+}
