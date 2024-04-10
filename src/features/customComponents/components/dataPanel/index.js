d126a433a2484879bb49a539f46294d258b52c35
Hamid Heyde <heidarzadeh.hamidreza@gmail.com>
2024-04-10 10:09:18 -0700

    added passed mapped state to DataPanelContent

--- src/features/customComponents/components/dataPanel/index.js
+++ src/features/customComponents/components/dataPanel/index.js
@@ -6,7 +6,11 @@ export const DataPanel = ({ setPanel, maps, setMaps }) => {
   return (
     <div className="dataPanelWrapper">
       <DataPanelHeader />
-      <DataPanelContent setPanel={setPanel} maps={maps} setMaps={setMaps} />
+      <DataPanelContent
+        setPanel={setPanel}
+        maps={maps}
+        setMaps={setMaps}
+      />
     </div>
   )
 }
