d0899abf05e969f0088bb2db7041afa8837f4e70
Hamid Heyde <heidarzadeh.hamidreza@gmail.com>
2024-04-10 10:11:17 -0700

    dataItems to dataPanel component

--- src/features/customComponents/components/dataPanel/DataPanelContent.js
+++ src/features/customComponents/components/dataPanel/DataPanelContent.js
@@ -4,6 +4,26 @@ import { NoDataContent } from "./content/NoDataContent"
 import { DataPanelContentFunctions } from "./DataPanelContentFunctions"
 
 export const DataPanelContent = ({ setPanel, maps, setMaps }) => {
+  const starClickHandler = (id) => {
+    setMaps(
+      maps.map((map) =>
+        map.id === id ? { ...map, favorite: !map.favorite } : map
+      )
+    )
+  }
+
+  const pinClickHandler = (id) => {
+    let pinnedMaps = maps.filter((map) => map.pin)
+    const clickedMap = maps.filter((map) => map.id === id)[0]
+    clickedMap.pin = !clickedMap.pin
+
+    pinnedMaps = clickedMap.pin
+      ? [{ ...clickedMap }, ...pinnedMaps]
+      : pinnedMaps.filter((map) => map.id !== id)
+
+    setMaps([...pinnedMaps, ...maps.filter((map) => !map.pin)])
+  }
+
   return (
     <div className="dataPanelContentWrapper">
       <DataPanelContentFunctions
@@ -12,9 +32,20 @@ export const DataPanelContent = ({ setPanel, maps, setMaps }) => {
         setMaps={setMaps}
       />
       <div className="contentList">
-        {maps.length === 0 ? <NoDataContent /> : maps.map((map, index) => (
-          <DataContentItem key={`map_${index}`} map={map} />
-        ))}
+        {maps.length === 0 ? (
+          <NoDataContent />
+        ) : (
+          maps
+            .filter((map) => map.visible)
+            .map((map, index) => (
+              <DataContentItem
+                key={`map_${index}`}
+                map={map}
+                pinClickHandler={pinClickHandler}
+                starClickHandler={starClickHandler}
+              />
+            ))
+        )}
       </div>
     </div>
   )
