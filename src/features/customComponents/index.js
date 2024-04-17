aa011e9fd173311af1e7bbf3a0214c6914c9aade
Hamid Heyde <heidarzadeh.hamidreza@gmail.com>
2024-04-17 15:28:11 -0700

    added alert structure

--- src/features/customComponents/index.js
+++ src/features/customComponents/index.js
@@ -3,16 +3,35 @@ import { useState } from "react"
 import { MapData } from "./data/MapsData"
 import { PanelHeader } from "./components/mainPanel/PanelHeader"
 import { PanelContent } from "./components/mainPanel/PanelContent"
+import { AppAlert } from "./components/AppAlert"
 
 export const CustomComponents = () => {
   const [maps, setMaps] = useState(MapData || [])
+  const [showAlert, setShowAlert] = useState({
+    type: "",
+    message: "",
+    visible: false,
+  })
 
   return (
     <div className="customComponentsAppWrapper">
       <div className="customComponentsWrapper">
         <div className="panelWrapper">
           <PanelHeader />
-          <PanelContent maps={maps} setMaps={setMaps} />
+          <PanelContent
+            maps={maps}
+            setMaps={setMaps}
+            showAlert={showAlert}
+            setShowAlert={setShowAlert}
+          />
+          {showAlert?.visible && (
+            <AppAlert
+              type={showAlert.type}
+              message={showAlert.message}
+              visible={showAlert.visible}
+              onclose={() => setShowAlert({ ...showAlert, visible: false })}
+            />
+          )}
         </div>
       </div>
     </div>
