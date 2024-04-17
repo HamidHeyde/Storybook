039d024c586d86ea5f466e80fcd992ebcd0f719b
Hamid Heyde <heidarzadeh.hamidreza@gmail.com>
2024-04-17 15:43:37 -0700

    moved functions to util

--- src/features/customComponents/components/dataPanel/content/DataContentItemDetails.js
+++ src/features/customComponents/components/dataPanel/content/DataContentItemDetails.js
@@ -5,73 +5,12 @@ import { MapAutoComplete } from "../../AutoComplete"
 import { MapBubbleSlider } from "../../MapBubbleSlider"
 import { MapColorSlider } from "../../MapColorSlider"
 import { MapShadowSlider } from "../../MapShadowSlider"
-
-const dynamicFieldsOptions = [
-  {
-    value: "50a85cc7-1602-4d8a-872b-40d0a27ad725",
-    text: "incident records ",
-    index: 0,
-  },
-  {
-    value: "31e63d5c-dbc2-43a0-bb8e-aa57961fecab",
-    text: "Production Capacity",
-    index: 1,
-  },
-  {
-    value: "21664875-8697-4459-a61f-974aa8f2e845",
-    text: "Upgrade Schedule",
-    index: 2,
-  },
-  {
-    value: "71675fd5-334c-4add-ae9c-b13432f64fdc",
-    text: "Mining Sites",
-    index: 3,
-  },
-  {
-    value: "4b1f1e15-58c1-4e39-9b0e-078b758435b7",
-    text: "Expenses",
-    index: 4,
-  },
-  {
-    value: "3f7aa830-51d5-4462-b9d2-33a1daf9bb4d",
-    text: "Revenue",
-    index: 5,
-  },
-  {
-    value: "5e7e6990-5280-4be4-ae53-46634637e58f",
-    text: "Number of employees",
-    index: 6,
-  },
-  {
-    value: "a75c55ac-a01d-447e-aed3-d0d8972e6502",
-    text: "Number of Assets",
-    index: 7,
-  },
-]
-
-const operationsOptions = [
-  { value: "count", text: "Count", index: 0 },
-  { value: "avg", text: "Average", index: 1 },
-  { value: "min", text: "Minimum", index: 2 },
-  { value: "max", text: "Maximum", index: 3 },
-]
-
-const getMetric = (metrics, ref, key) => {
-  const theMetric = metrics.filter((metric) => metric["ref"] === ref)
-
-  return !!theMetric?.length ? theMetric[0][key] : undefined
-}
-
-const getOptions = (optionsList, metrics, ref, key) => {
-  const metricValue = getMetric(metrics, ref, key)
-  const optionItem = optionsList.filter(
-    (option) =>
-      option?.text?.toLowerCase() === metricValue?.toLowerCase() ||
-      option?.value?.toLowerCase() === metricValue?.toLowerCase()
-  )
-
-  return !!optionItem?.length ? optionItem[0] : undefined
-}
+import {
+  dynamicFieldsOptions,
+  operationsOptions,
+  getMetric,
+  getOptions,
+} from "../../utils/utils"
 
 export const DataContentItemDetails = ({ map }) => {
   const mapMetrics = map.metrics
