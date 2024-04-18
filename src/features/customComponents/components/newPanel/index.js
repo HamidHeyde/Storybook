39897a889963eeadcff44cb6590fd4e81c332301
Hamid Heyde <heidarzadeh.hamidreza@gmail.com>
2024-04-18 13:02:03 -0700

    new panel component

--- src/features/customComponents/components/newPanel/index.js
+++ src/features/customComponents/components/newPanel/index.js
@@ -1,7 +1,79 @@
-import './index.css'
+import "./index.css"
+import { useState } from "react"
+import { NewPanelContent } from "./NewPanelContent"
+import { NewPanelActions } from "./NewPanelActions"
+import { getFormValidationMessage, getRequestData } from "../utils/utils"
 
-export const NewPanel = () => {
+export const NewPanel = ({ setPanel, maps, setMaps, showAlert, setShowAlert }) => {
+  const [formFields, setFormFields] = useState({
+    numberSlider: [],
+    sizeSlider: [0, 25, 50, 75, 100],
+    colorSlider: [0, 35, 100],
+    shadowSlider: [0, 35, 65, 100],
+  })
+
+  const handleOnChange = (e) => {
+    const fieldName = e.target.name
+    const fieldValue = e.target.value
+    const fieldLabel = e.target.text
+
+    setFormFields((prev) => ({
+      ...prev,
+      [fieldName]: fieldValue,
+      ...(!!fieldLabel && { [`${fieldName}Label`]: fieldLabel }),
+    }))
+  }
+
+  const handleOnAdd = () => {
+    const validationMessage = getFormValidationMessage(formFields)
+    console.log(validationMessage)
+    if (validationMessage) {
+      setShowAlert({
+        ...{
+          type: "error",
+          message: validationMessage,
+          visible: true,
+        },
+      })
+      return
+    }
+
+    if (!validationMessage) {
+      const requestData = {
+        ...getRequestData(formFields),
+        id: maps.length + 1,
+        pin: false,
+        favorite: false,
+        visible: true,
+      }
+
+      setMaps([
+        ...maps,
+        {
+          ...requestData,
+        },
+      ])
+
+      setShowAlert({
+        ...{
+          type: "success",
+          message: "New Panel Added Successfully",
+          visible: true,
+        },
+      })
+
+      setPanel("DataPanel")
+    }
+  }
+
+  const handleOnCancel = () => {
+    setPanel("DataPanel")
+  }
+
   return (
-    <div className="newPanelWrapper">New Map Panel</div>
+    <div className="newPanelWrapper">
+      <NewPanelContent onChange={handleOnChange} />
+      <NewPanelActions onAdd={handleOnAdd} onCancel={handleOnCancel} />
+    </div>
   )
 }
