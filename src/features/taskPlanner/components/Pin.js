6838dda3c0df93100d84b3fd12018e294a7bad1d
Hamid Heyde <heidarzadeh.hamidreza@gmail.com>
2024-04-24 19:52:03 -0700

    added size to arguments

--- src/features/taskPlanner/components/Pin.js
+++ src/features/taskPlanner/components/Pin.js
@@ -1,12 +1,18 @@
-import './pin.css'
+import "./pin.css"
+import PropTypes from "prop-types"
 import PushPinIcon from "@mui/icons-material/PushPin"
 
-export const Pin = ({ id, pinned, onClick }) => {
-  const pinClass = pinned ? "pinned" : "notPinned"
+
+/**
+ * Renders a pin component with the specified properties.
+ */
+export const Pin = ({ id, pinned, onClick, size }) => {
+  const pinClass = !!pinned ? "pinned" : "notPinned"
 
   const pinProps = {
     className: ["pinBase", pinClass].join(" "),
     ...(onClick && { onClick: () => onClick(id) }),
+    style: { "font-size": size ? `${size}rem` : '1.5rem' },
   }
 
   return (
@@ -16,3 +22,10 @@ export const Pin = ({ id, pinned, onClick }) => {
   )
 }
 
+// PropTypes
+Pin.propTypes = {
+  id: PropTypes.number,
+  pinned: PropTypes.bool,
+  onClick: PropTypes.func,
+  size: PropTypes.number,
+}
