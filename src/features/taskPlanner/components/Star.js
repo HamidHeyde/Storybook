5688b50f021a5e7c4d56fb5eca8fc100c020f332
Hamid Heyde <heidarzadeh.hamidreza@gmail.com>
2024-04-24 19:55:12 -0700

    updated the Start properties

--- src/features/taskPlanner/components/Star.js
+++ src/features/taskPlanner/components/Star.js
@@ -1,12 +1,14 @@
 import "./star.css"
+import PropTypes from "prop-types"
 import StarIcon from "@mui/icons-material/Star"
 
-export const Star = ({ id, favorite, onClick }) => {
+export const Star = ({ id, favorite, onClick, size }) => {
   const starClass = favorite ? "starred" : "notStarred"
 
   const starProps = {
     className: ["starBase", starClass].join(" "),
     ...(onClick && { onClick: () => onClick(id) }),
+    style: { "font-size": size ? `${size}rem` : "1.5rem" },
   }
 
   return (
@@ -14,4 +16,12 @@ export const Star = ({ id, favorite, onClick }) => {
       <StarIcon {...starProps} />
     </div>
   )
+}
+
+// PropTypes
+Star.propTypes = {
+  id: PropTypes.number,
+  pinned: PropTypes.bool,
+  onClick: PropTypes.func,
+  size: PropTypes.number,
 }
