e435d8ba3a02613dcea86e4804d523e09fdb360f
Hamid Heyde <heidarzadeh.hamidreza@gmail.com>
2024-04-24 19:59:01 -0700

    Story for star component

---
+++ src/features/taskPlanner/components/Star.stories.js
@@ -0,0 +1,32 @@
+import { fn } from "@storybook/test"
+import { Star } from "./Star"
+import { storybookSettings } from "../storybook/globals"
+
+export default {
+  title: "TaskPlanner/Elements/Star",
+  component: Star,
+  parameters: {
+    ...storybookSettings("centered"),
+    controls: {
+      exclude: ["size"],
+    },
+  },
+  tags: ["autodocs"],
+  render: (args) => <Star {...args} />,
+}
+
+export const Default = {
+  args: {
+    id: 1,
+    favorite: false,
+    onClick: fn(),
+    size: 5,
+  },
+}
+
+export const Favorite = {
+  args: {
+    ...Default.args,
+    favorite: true,
+  },
+}
