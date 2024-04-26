48dc13e73940703917c2b50c70d17ebaf457db01
Hamid Heyde <heidarzadeh.hamidreza@gmail.com>
2024-04-25 18:31:42 -0700

    Button story

---
+++ src/features/taskPlanner/components/Button.stories.js
@@ -0,0 +1,22 @@
+import { fn } from "@storybook/test"
+import { Button } from "./Button"
+import { storybookSettings } from "../storybook/globals"
+
+export default {
+  title: "TaskPlanner/Elements/Button",
+  component: Button,
+  parameters: {
+    ...storybookSettings("centered"),
+  },
+  tags: ["autodocs"],
+  render: (args) => <Button {...args} />,
+}
+
+export const Default = {
+  args: {
+    width: 200,
+    text: "Button Text",
+    onClick: fn(),
+  },
+}
+
