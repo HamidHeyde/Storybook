4cf93336b1e87ff205aec7495897f8122fab3ec9
Hamid Heyde <heidarzadeh.hamidreza@gmail.com>
2024-04-25 18:40:39 -0700

    added propTypes

--- src/features/taskPlanner/components/TextInput.js
+++ src/features/taskPlanner/components/TextInput.js
@@ -1,10 +1,19 @@
 import './textInput.css'
+import PropTypes from "prop-types"
 
-export const TextInput = ({ value, onChange }) => (
+export const TextInput = ({ width, value, onChange }) => (
   <input
     className="addTodoInput"
+    style={{ width: `${width}%` || '100%' }}
     type="text"
     value={value}
     onChange={onChange}
   />
 )
+
+// PropTypes
+TextInput.propTypes = {
+  width: PropTypes.number,
+  value: PropTypes.string,
+  onChange: PropTypes.func,
+}
