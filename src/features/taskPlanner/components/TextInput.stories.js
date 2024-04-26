81ddc9a985f8bc5e5d035a76a7e5f3165c9e3a62
Hamid Heyde <heidarzadeh.hamidreza@gmail.com>
2024-04-25 18:40:58 -0700

    TextInput Story

---
+++ src/features/taskPlanner/components/TextInput.stories.js
@@ -0,0 +1,21 @@
+import { fn } from "@storybook/test"
+import { TextInput } from "./TextInput"
+import { storybookSettings } from "../storybook/globals"
+
+export default {
+  title: "TaskPlanner/Elements/TextInput",
+  component: TextInput,
+  parameters: {
+    ...storybookSettings("centered"),
+  },
+  tags: ["autodocs"],
+  render: (args) => <TextInput {...args} />,
+}
+
+export const Default = {
+  args: {
+    width: 200,
+    value: "Some text value",
+    onChange: fn(),
+  },
+}
