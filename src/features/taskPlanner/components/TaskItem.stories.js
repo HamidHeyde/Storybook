fbc34a9db5c52dcf7a9180df9b1d621c81dc7b0e
Hamid Heyde <heidarzadeh.hamidreza@gmail.com>
2024-04-28 17:21:50 -0700

    renamed the component

---
+++ src/features/taskPlanner/components/TaskItem.stories.js
@@ -0,0 +1,35 @@
+import { fn } from "@storybook/test"
+import { TaskItem } from "./TaskItem"
+import { storybookSettings } from "../storybook/globals"
+
+export default {
+  title: "TaskPlanner/Compounds/TaskItem",
+  component: TaskItem,
+  parameters: {
+    ...storybookSettings("centered"),
+  },
+  tags: ["autodocs"],
+  decorators: [
+    (Story) => (
+      <div style={{ width: "500px" }}>
+        <Story />
+      </div>
+    ),
+  ],
+  render: (args) => <TaskItem {...args} />,
+}
+
+export const Default = {
+  args: {
+    todoItem: {
+      id: 1,
+      title: "1st One",
+      completed: false,
+      favorite: false,
+      pinned: false,
+    },
+    onStarClickHandler: fn(),
+    onPinClickHandler: fn(),
+    onCheckboxClickHandler: fn(),
+  },
+}
