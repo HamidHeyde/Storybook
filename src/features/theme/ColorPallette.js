ef2ae1de4dde48c7546f7accd8f7cb1d236b6c74
Hamid Heyde <heidarzadeh.hamidreza@gmail.com>
2024-05-06 09:35:48 -0700

    Color Palette

---
+++ src/features/theme/ColorPallette.js
@@ -0,0 +1,44 @@
+export const red = {
+  100: "#fee2e2",
+  200: "#fecaca",
+  300: "#fca5a5",
+  400: "#f87171",
+  500: "#ef4444",
+  600: "#dc2626",
+}
+
+export const orange = {
+  100: "#ffedd5",
+  200: "#fed7aa",
+  300: "#fdba74",
+  400: "#fb923c",
+  500: "#f97316",
+  600: "#ea580c",
+}
+
+export const yellow = {
+  100: "#fef9c3",
+  200: "#fef08a",
+  300: "#fde047",
+  400: "#facc15",
+  500: "#eab308",
+  600: "#ca8a04",
+}
+
+export const green = {
+  100: "#dcfce7",
+  200: "#bbf7d0",
+  300: "#86efac",
+  400: "#4ade80",
+  500: "#22c55e",
+  600: "#16a34a",
+}
+
+export const blue = {
+  100: "#dbeafe",
+  200: "#bfdbfe",
+  300: "#93c5fd",
+  400: "#60a5fa",
+  500: "#3b82f6",
+  600: "#2563eb",
+}
