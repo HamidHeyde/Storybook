6b308ecba9df589b7e5d5560f5169a9cbda16622
Hamid Heyde <heidarzadeh.hamidreza@gmail.com>
2024-05-06 11:48:16 -0700

    layout story

---
+++ src/features/Layouts/LeftMenuRightContentLayout.stories.js
@@ -0,0 +1,55 @@
+import { fn } from "@storybook/test"
+import { storybookSettings } from "./storybook/globals"
+import { LeftMenuRightContentLayout } from "./LeftMenuRightContentLayout"
+import { ReactComponent as Map } from "../customComponents/assets/map.svg"
+
+export default {
+  title: "4Layouts/Left Menu Right Content",
+  component: LeftMenuRightContentLayout,
+  parameters: {
+    ...storybookSettings(""),
+    controls: {
+      exclude: ["selectedMenu"],
+    }
+  },
+  tags: ["autodocs"],
+  decorators: [
+    (Story) => (
+      <div
+        style={{
+          boxSizing: "border-box",
+          width: "100vw",
+          height: "100vh",
+          display: "flex",
+          flexDirection: "row",
+        }}
+      >
+        <Story />
+      </div>
+    ),
+  ],
+  render: (args) => (
+    <>
+      <LeftMenuRightContentLayout {...args}>
+        {"The Content"}
+      </LeftMenuRightContentLayout>
+    </>
+  ),
+}
+
+export const Default = {
+  args: {
+    menuItems: [
+      {
+        name: `Sample Menu Item 1`,
+        icon: Map,
+      },
+      {
+        name: `Sample Menu Item 2`,
+        icon: Map,
+      },
+    ],
+    selectedMenu: "Sample Menu Item 2",
+    setSelectedMenu: fn(),
+  },
+}
