f551f54e3357da3e46cdd280d5b9ea96a02ad19d
Hamid Heyde <heidarzadeh.hamidreza@gmail.com>
2024-05-08 07:38:38 -0700

    added background color to the component

--- src/features/customComponents/components/AddButton.js
+++ src/features/customComponents/components/AddButton.js
@@ -1,11 +1,16 @@
 import './addButton.css'
 import AddIcon from "@mui/icons-material/Add"
 
-export const AddButton = ({ onClick }) => {
+export const AddButton = ({ onClick, backgroundColor }) => {
   const addButtonProperties = {
     ...(onClick && { onClick }),
     className: "addButton", 
   }
 
-  return <AddIcon {...addButtonProperties} />
+  return (
+    <AddIcon
+      {...addButtonProperties}
+      style={( backgroundColor && {color: backgroundColor })}
+    />
+  )
 }
