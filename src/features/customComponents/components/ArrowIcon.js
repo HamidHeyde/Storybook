b4b2aba777e56eb484c845bbb8844610119f54f2
Hamid Heyde <heidarzadeh.hamidreza@gmail.com>
2024-05-09 12:05:57 -0700

    added styling

--- src/features/customComponents/components/ArrowIcon.js
+++ src/features/customComponents/components/ArrowIcon.js
@@ -1,16 +1,14 @@
 import "./arrowIcon.css"
 import { ReactComponent as ArrowDown } from "../assets/arrowDown.svg"
 
-export const ArrowIcon = ({ isOpen, setIsOpen }) => {
+export const ArrowIcon = ({ isOpen, setIsOpen, backgroundColor }) => {
   const toggleHandler = () => setIsOpen((prev) => !prev)
-  
+
   return (
     <ArrowDown
-      className={[
-        "arrowDownIcon",
-        isOpen ? "arrowDownIconOpen" : "",
-      ].join(" ")}
+      className={["arrowDownIcon", isOpen ? "arrowDownIconOpen" : ""].join(" ")}
       onClick={toggleHandler}
+      style={{ fill: backgroundColor || "teal"}}
     />
   )
 }
