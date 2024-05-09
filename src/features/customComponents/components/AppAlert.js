f85b07a763052309cec8bc9bcd577bd499da9fa7
Hamid Heyde <heidarzadeh.hamidreza@gmail.com>
2024-05-09 08:16:20 -0700

    added proptypes

--- src/features/customComponents/components/AppAlert.js
+++ src/features/customComponents/components/AppAlert.js
@@ -1,6 +1,7 @@
 import "./appAlert.css"
 import { useState, useEffect } from "react"
 import Alert from "@mui/material/Alert"
+import propTypes from "prop-types"
 
 export const AppAlert = ({ type, message, visible, onclose }) => {
   const [visibleState, setVisibleState] = useState(visible)
@@ -20,10 +21,23 @@ export const AppAlert = ({ type, message, visible, onclose }) => {
   }, [setVisibleState])
 
   return (
-    <div className="alertWrapper" style={{ opacity: visibleState ? 1 : 0 }}>
+    <div
+      className="alertWrapper"
+      style={{
+        opacity: visibleState ? 1 : 0
+      }}
+    >
       <Alert style={{ width: "97%" }} severity={type}>
         {message}
       </Alert>
     </div>
   )
+}
+
+// propTypes
+AppAlert.propTypes = {
+  type: propTypes.oneOf(["success", "info", "warning", "error"]).isRequired,
+  message: propTypes.string.isRequired,
+  visible: propTypes.bool.isRequired,
+  onclose: propTypes.func
 }
