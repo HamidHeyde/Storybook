dad9182b208d4a69e9221cf6a9fc57a99652aaa8
Hamid Heyde <heidarzadeh.hamidreza@gmail.com>
2024-05-11 12:10:57 -0700

    added propTypes for mapLabel

--- src/features/customComponents/components/MapLabel.js
+++ src/features/customComponents/components/MapLabel.js
@@ -1,7 +1,15 @@
 import "./mapLabel.css"
+import propTypes from "prop-types"
 
 export const MapLabel = ({ type, width, text }) => (
   <div className={type === "title" ? "titleText" : "normalText"} style={{width}}>
     {text}
   </div>
 )
+
+// propTypes
+MapLabel.propTypes = {
+  type: propTypes.string,
+  width: propTypes.string,
+  text: propTypes.string,
+}
